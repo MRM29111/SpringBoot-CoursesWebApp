67d3a6c9ea4253c6d596fbdc0e01831e8948cfbd
Zavada <volodymyrzavada@gmail.com>
2018-03-02 17:21:13 +0200

    Refactoring. Security check combination of email&password.

--- src/main/java/com/zavada/domain/LoginRequest.java
+++ src/main/java/com/zavada/domain/LoginRequest.java
@@ -1,14 +1,11 @@
 package com.zavada.domain;
 
-import com.zavada.validator.CheckEmailPasswordCombination;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
 @Getter @Setter
-@CheckEmailPasswordCombination
 public class LoginRequest {
 
 	private String email;
